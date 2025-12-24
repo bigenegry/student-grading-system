c422d228f4c830b91d510102fce1eea6f9bc5b61
bigenegry <nwaerick101@gmail.com>
2025-12-24 16:41:30 +0000

    Refactor Person class header and remove comments
    Removed comments and updated getter method definitions.

--- src/Person.h
+++ src/Person.h
@@ -1,6 +1,3 @@
-// Person class header
-// Created by bigenegry
-
 #ifndef PERSON_H
 #define PERSON_H
 
@@ -17,31 +14,25 @@ private:
     double finalGrade;
 
 public:
-    // Constructors
     Person();
     Person(const std::string& first, const std::string& last);
-    
-    // Rule of Three
+
     Person(const Person& other);
     Person& operator=(const Person& other);
     ~Person();
 
-    // Input/Output methods
     friend std::istream& operator>>(std::istream& is, Person& person);
     friend std::ostream& operator<<(std::ostream& os, const Person& person);
 
-    // Grade calculation
     void calculateFinalGrade(bool useAverage);
-    
-    // Getters
-    std::string getFirstName() const { return firstName; }
-    std::string getLastName() const { return lastName; }
-    double getFinalGrade() const { return finalGrade; }
 
-    // Utility methods
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    double getFinalGrade() const;
+
     void addHomework(int score);
     void setExam(int score);
-    
+
 private:
     double calculateAverage() const;
     double calculateMedian() const;
